0d3d68fffe295ebae0f732b9df7439f7b1b52397
Ted Wei <jdwei@hotmail.com>
2016-12-27 15:38:30 +0800

    finish list page

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { Locations } from '../../providers/locations';
 
-/*
-  Generated class for the List page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public locations: Locations) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ListPage');
+    console.log('Hello ListPage Page');
   }
 
 }
