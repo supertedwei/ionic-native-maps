8e712c2b948ea8a3e3a9141d357a25b7b5faa411
Ted Wei <jdwei@hotmail.com>
2016-12-27 11:02:24 +0800

    home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,50 +1,18 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
- 
+import { MapPage } from '../map/map';
+import { ListPage } from '../list/list';
+
 @Component({
-  selector: 'home-page',
+  selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
- 
-    map: GoogleMap;
- 
-    constructor(public navCtrl: NavController, public platform: Platform) {
-        platform.ready().then(() => {
-            this.loadMap();
-        });
-    }
- 
-    loadMap(){
- 
-        let location = new GoogleMapsLatLng(-34.9290,138.6010);
- 
-        this.map = new GoogleMap('map', {
-          'backgroundColor': 'white',
-          'controls': {
-            'compass': true,
-            'myLocationButton': true,
-            'indoorPicker': true,
-            'zoom': true
-          },
-          'gestures': {
-            'scroll': true,
-            'tilt': true,
-            'rotate': true,
-            'zoom': true
-          },
-          'camera': {
-            'latLng': location,
-            'tilt': 30,
-            'zoom': 15,
-            'bearing': 50
-          }
-        });
- 
-        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-            console.log('Map is ready!');
-        });
- 
-    }
+
+  tab1Root: any = MapPage;
+  tab2Root: any = ListPage;
+
+  constructor(){
+
+  }
+
 }
